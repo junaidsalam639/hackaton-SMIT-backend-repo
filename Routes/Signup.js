51cea5882a34ecc81f724d8ed5653f88c723d24e
junaidhnhtechsolutions <junaid@hnhtechsolutions.com>
2024-01-29 12:59:26 +0500

    'mongoose'

--- Routes/Signup.js
+++ Routes/Signup.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const route = express.Router();
 const Signup = require('../Schema/Signup');
+const jwt = require('jsonwebtoken');
+
 
 route.get('/', async (req, res) => {
     const signup = await Signup.find();
@@ -33,6 +35,31 @@ route.post('/', async (req, res) => {
             status : 200,
             signup : signup
         });
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+route.post('/login', async (req, res) => {
+    try{
+        const signup = await Signup.findOne({email : req.body.email , password : req.body.password});
+        if(signup){
+            const token = await jwt.sign({
+                data: signup
+            }, 'JFKJEKLJREKLNHRKLEJTHRJKLTHEJL');
+            res.send({
+                status : 200,
+                message : 'login successfully',
+                signup : signup,
+                token : token
+            });
+        }else{
+            res.send({
+                status : 404,
+                error : 'user not found'
+            });
+        }
     }catch(err){
         console.log(err);
     }
--- index.js
+++ index.js
@@ -9,12 +9,11 @@ app.use(express.json());
 app.use(cors());
 app.use('/signup' , Signup);
 
-mongoose.connect('mongodb+srv://Blog:blog@cluster0.4t24tdb.mongodb.net/').then(()=>{
+mongoose.connect('mongodb+srv://junaid:junaid@cluster0.isz5lnm.mongodb.net/').then(()=>{
     console.log('mongodb connect');
 }).catch((err)=> {
     console.log(err);
-})
-
+});
 
 app.get('/' , (req , res) => {
     res.send('Hello World');
